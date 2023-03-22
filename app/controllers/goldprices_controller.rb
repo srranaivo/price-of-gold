aadf88e4b07825f23f0913cf676a6dbf199bc53f
sophie ramahenina <ramahenina.sophie@gmail.com>
2023-03-22 17:57:15 +0100

    add goldprices_controller

--- app/controllers/goldprices_controller.rb
+++ app/controllers/goldprices_controller.rb
@@ -1,6 +1,7 @@
 class GoldpricesController < ApplicationController
 
   #ici il faut coder la méthode pour récupérer le prix de l'or dans la devise sélectionnée par l'utilisateur
+
   def index
     @goldprices = Goldprice.all
   end
